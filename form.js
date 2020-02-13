c663db429787b94b95884777a99b10cc99bb5cb9
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-13 15:39:49 -0700

    added axios post request

--- form.js
+++ form.js
@@ -1,16 +1,22 @@
-//when DOM loads 
+import Axios from "axios";
+
+//when DOM loads
 document.addEventListener("DOMContentLoaded", () => {
-    //this looks for the form submission event
-    document.querySelector("form").addEventListener("submit", (event) => {
-        event.stopPropagation();
-        event.preventDefault(); //stops page from auto reloading page 
+  //this looks for the form submission event
+  document.querySelector("form").addEventListener("submit", event => {
+    event.stopPropagation();
+    event.preventDefault(); //stops page from auto reloading page
 
-        //select form data from DOM
-        const name = document.querySelectorAll('form input[name="name"]')[0].value;
-        const email = document.querySelectorAll('form input[name="email"]')[0].value;
+    //select form data from DOM
+    const name = document.querySelectorAll('form input[name="name"]')[0].value;
+    const email = document.querySelectorAll('form input[name="email"]')[0]
+      .value;
 
-        console.log(name, email);
-        
+    console.log(name, email);
 
-    }) 
-})
+    axios.post("/form", {
+      name,
+      email
+    });
+  });
+});
